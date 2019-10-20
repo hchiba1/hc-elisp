94fb1aa30e3763c15f55d6a4b2c8046ff36eab19
Hirokazu Chiba <hchiba1@gmail.com>
2019-10-20 21:47:33 +0900

    add .emacs.el

---
+++ .emacs.el
@@ -0,0 +1,13 @@
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+;; (package-initialize)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+(load "~/etc/hc-elisp/hc-index")
