0287a2823f92d1cb2bdb639df8fb3e9ee78059f8
Hirokazu Chiba <hchiba1@gmail.com>
2024-12-24 15:41:28 +0900

    Add nginx-mode

--- hc-programming.el
+++ hc-programming.el
@@ -457,3 +457,5 @@ With argument ARG, do this that many times."
   (add-hook 'prog-mode-hook 'copilot-mode)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
+
+(require 'nginx-mode)
